88d6969fe0a63b35d0d1a0275b06142704516b85
Arif Mahmud Pranto <pranto48@gmail.com>
2025-12-15 11:07:22 +0600

    Fix: Include subchoice field in get_public_map_data for proper icon rendering

--- portal.itsupport.com.bd/docker-ampnm/api.php
+++ portal.itsupport.com.bd/docker-ampnm/api.php
@@ -23,7 +23,7 @@ try {
 
         $stmt_devices = $pdo->prepare("
             SELECT 
-                d.id, d.name, d.ip, d.check_port, d.type, d.description, d.x, d.y, 
+                d.id, d.name, d.ip, d.check_port, d.type, d.subchoice, d.description, d.x, d.y, 
                 d.ping_interval, d.icon_size, d.name_text_size, d.icon_url, 
                 d.warning_latency_threshold, d.warning_packetloss_threshold, 
                 d.critical_latency_threshold, d.critical_packetloss_threshold, 
